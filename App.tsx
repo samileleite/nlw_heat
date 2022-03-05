219e0b16baea7d18b46530a99f5bf1285991bdcc
Samile Leite <samileleite77@gmail.com>
2022-03-05 10:47:43 -0300

    feat: app nlw heat initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
+import React from 'react';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { AuthProvider } from './src/hooks/auth';
+import { Home } from './src/screens/Home';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Each child in a list should have a unique "key" prop.']);
+LogBox.ignoreLogs(['Each child in a list should have a unique "key" prop.']);
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <StatusBar
+        style="light"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Home />
+    </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
